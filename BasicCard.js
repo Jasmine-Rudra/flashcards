b1be3aaf8faae65c504ee5d525c6d9f50b743cf3
Jasmine-Rudra <rudrajasmine@gmail.com>
2017-09-25 16:07:07 -0700

    basic card complete

---
+++ BasicCard.js
@@ -0,0 +1,18 @@
+const fs=require("fs");
+var BasicCard= new Function(front, back){
+	if(this instanceof BasicCard){
+		this.front=front;
+		this.back=back;
+		fs.appendFile("basicCard.csv", this.front + "," + this.back + "\n", "utf8", function(error) {
+    		if(err){
+    			return console.log(err);
+    		}
+		})
+	}
+	else{
+		return new BasicCard(front,back);
+	}
+
+}
+
+module.exports=BasicCard;
